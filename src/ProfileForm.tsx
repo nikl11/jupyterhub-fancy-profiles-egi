ed6a2a84df8e2776bac1fe800d9e09bf37ef8512
Vojtech Nikl <nikl@cesnet.cz>
2026-02-06 16:18:28 +0000

    UI update fp10

--- src/ProfileForm.tsx
+++ src/ProfileForm.tsx
@@ -85,7 +85,7 @@ export function ProfileForm() {
 
         <div className="fp-launch">
           <button className="btn btn-jupyter fp-launch-btn" type="submit">
-            Launch
+            Launch ZyZ
           </button>
         </div>
       </div>
