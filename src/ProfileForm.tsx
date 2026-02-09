c2524b6730d768df9d91c67148b654ce7e39584c
Vojtech Nikl <nikl@cesnet.cz>
2026-02-09 12:12:19 +0000

    Revert "update test 2"
    This reverts commit fdbd7ada14f11429fbee4865bb1067dd6c0ebdfe.

--- src/ProfileForm.tsx
+++ src/ProfileForm.tsx
@@ -53,7 +53,7 @@ export function ProfileForm() {
         <div className="fp-row">
           <div className="fp-label">
             <label htmlFor="fp-server-option" className="form-label">
-              Server option ZzZ
+              Server option
             </label>
           </div>
 
