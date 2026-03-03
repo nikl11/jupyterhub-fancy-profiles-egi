5909117e48f6450803bd03fa94ac692e4e9ad106
Vojtech Nikl <nikl@cesnet.cz>
2026-03-03 10:17:08 +0000

    UI update fp109

--- src/ProfileForm.tsx
+++ src/ProfileForm.tsx
@@ -114,7 +114,7 @@ function OptionsPreview(props: { selectedSlug: string }) {
         </div>
 
         <button className="btn btn-jupyter form-control mt-3" type="submit">
-          Start ZzZzZzZz
+          Start_ZzZzZzZz
         </button>
       </div>
     </div>
