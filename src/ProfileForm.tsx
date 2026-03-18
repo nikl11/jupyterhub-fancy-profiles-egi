9e16c4968d2a3d1c6f87aebaf749f8515a947f1a
Vojtech Nikl <nikl@cesnet.cz>
2026-03-18 18:35:17 +0000

    UI update fp192

--- src/ProfileForm.tsx
+++ src/ProfileForm.tsx
@@ -426,7 +426,7 @@ function BuildAndLaunch(props: {
             {isBuilding ? "Building..." : "Build image"}
           </button>
 
-          <button type="button" className="btn btn-outline-dark" onClick={buildControls.toggleLogs}>
+          <button type="button" className="btn btn-outline-secondary" onClick={buildControls.toggleLogs}>
             {buildState.logsOpen ? "Close logs" : "Open logs"}
           </button>
 
